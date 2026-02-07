299202fc020b19e7dc9b436d94e24d627179ea6a
fitz <whitfordfilart@gmail.com>
2026-02-07 23:03:21 +0000

    Character Class Update 07_02_2026

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("hi from fitz")
+class Character:
+    def __init__(self, name):
+        self.name = name
+        self.room = None
+        self.weapon = None
+        self.is_murderer = False
+
+    def assign_weapon(self, weapon):
+        """Assign a weapon to a character"""
+        self.weapon = weapon
+
+    def move_to_room(self, room):
+        """Move a character to a room"""
+        self.room = room
+
+    def is_in_room(self, room):
+        """Check if a character is in a room"""
+        return self.room == room
+
+    def init_murderer(self, is_murder = True):
+        """Initialise a character as the murderer"""
+        self.is_murderer = is_murder
+
+    def describe_state(self):
+        """Describe a character's state"""
+        if self.room:
+            room_name = self.room.name
+        else:
+            room_name = "No room"
+        if self.weapon:
+            weapon_name = self.weapon.name
+        else:
+            weapon_name = "No weapon"
+        print(f"{self.name}: Room = {room_name}, "
+              f"Weapon = {weapon_name}, "
+              f"Murderer = {self.is_murderer}")
+
