635e20996635d13f3642a49bea58274def078580
fitz <whitfordfilart@gmail.com>
2026-04-25 12:20:35 +0100

    test_player_py

--- tests/test_player.py
+++ tests/test_player.py
@@ -10,5 +10,15 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(player.col, 6)
         self.assertEqual(player.row, 5)
 
+    def test_player_move_through_door(self):
+        player = Player(5, 5)
+        doors = {(6, 5): "Kitchen"}
+        room_seats = {"Kitchen": [(10, 10), (11, 11)]}
+        result = player.move(1, 0, [], doors, room_seats)
+        self.assertEqual(result, "ENTERED_ROOM")
+        self.assertEqual(player.col, 10)
+        self.assertEqual(player.row, 10)
+        self.assertEqual(player.in_room, "Kitchen")
+
 if __name__ == '__main__':
     unittest.main()
