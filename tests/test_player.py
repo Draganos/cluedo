f4f570f4f3b4b7648f5a0a52622bef06138cb881
fitz <whitfordfilart@gmail.com>
2026-04-25 12:32:36 +0100

    test_player_py

--- tests/test_player.py
+++ tests/test_player.py
@@ -11,7 +11,7 @@ class TestPlayer(unittest.TestCase):
         self.assertEqual(player.row, 5)
 
     def test_player_enter_rooms(self):
-        test_cases = [
+        rooms = [
             ((6, 5), "Kitchen", (20, 20)),
             ((6, 3), "Study", (2, 1)),
             ((9, 4), "Hall", (11, 2)),
@@ -19,7 +19,7 @@ class TestPlayer(unittest.TestCase):
             ((6, 8), "Library", (2, 7)),
             ((8, 19), "Ballroom", (10, 19)),
         ]
-        for door_position, room_name, expected_position in test_cases:
+        for door_position, room_name, expected_position in rooms:
             player = Player(5, 5)
             doors = {door_position: room_name}
             room_seats = {room_name: [expected_position]}
