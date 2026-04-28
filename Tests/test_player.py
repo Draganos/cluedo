be7fd7b424eb360b757e520768d1b446f1eb0969
fitz <whitfordfilart@gmail.com>
2026-04-28 17:49:21 +0100

    update

--- Tests/test_player.py
+++ Tests/test_player.py
@@ -5,14 +5,14 @@ class TestPlayer(unittest.TestCase):
 
     def test_player_movement(self):
         player = Player(5, 5)
-        result = player.move(1, 0, [], {}, {})
+        result = player.move(1, 0, [], {}, {}, {})
         self.assertEqual(result, "MOVED")
         self.assertEqual(player.col, 6)
         self.assertEqual(player.row, 5)
 
     def test_player_out_of_bounds(self):
         player = Player(0, 0)
-        result = player.move(-1, 0, [], {}, {})
+        result = player.move(-1, 0, [], {}, {}, {})
         self.assertEqual(player.col, 0)
         self.assertEqual(player.row, 0)
         self.assertIsNone(result)
@@ -20,7 +20,7 @@ class TestPlayer(unittest.TestCase):
     def test_player_forbidden_zone(self):
         player = Player(5, 5)
         forbidden = [(6, 5)]
-        result = player.move(1, 0, forbidden, {}, {})
+        result = player.move(1, 0, forbidden, {}, {}, {})
         self.assertEqual(result, None)
         self.assertEqual(player.col, 5)
         self.assertEqual(player.row, 5)
@@ -37,14 +37,13 @@ class TestPlayer(unittest.TestCase):
             ((16, 12), "Dining Room", (19, 10)),
             ((4, 19), "Conservatory", (2, 20)),
         ]
-
         for door_position, room_name, expected_position in rooms:
             player = Player(5, 5)
             doors = {door_position: room_name}
             room_seats = {room_name: [expected_position]}
             dx = door_position[0] - 5
             dy = door_position[1] - 5
-            result = player.move(dx, dy, [], doors, room_seats)
+            result = player.move(dx, dy, [], doors, room_seats, {})
             self.assertEqual(result, "ENTERED_ROOM")
             self.assertEqual((player.col, player.row), expected_position)
             self.assertEqual(player.in_room, room_name)
